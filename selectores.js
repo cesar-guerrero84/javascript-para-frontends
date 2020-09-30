ff39db25a1611c79f1f334de5b3894370b322f22
cesar guerrero <guerrerofernandez007@gmail.com>
2020-09-30 11:08:02 -0500

    se cambio el js por selectores y se finalizo la seccion de selectores

--- selectores.js
+++ selectores.js
@@ -38,4 +38,29 @@ var ñ = document.getElementsByTagName("p")[2]; // aqui usamos el getelementsbyt
 
 ñ.style.padding = "50px"; // aqui tomamos la var ñ que contiene el p de la posicion 2 y le agregamos padding de 50px
 
-ñ.innerHTML = "cambie el texto gracias al metodo innerHTML"; // el inner html es un metodo de js para acceder a un elemento html y modificarlo en este caso modificamos el p de la posicion 2
+ñ.innerHTML = "cambie el texto gracias al metodo innerHTML "; // el inner html es un metodo de js para acceder a un elemento html y modificarlo en este caso modificamos el p de la posicion 2
+
+ñ.innerHTML += "agrege un nuevo texto con el operador +="; // con el operador += y el innerHTML se agrega un nuevo texto al parrafo de la var ñ
+
+k.innerHTML += "<h4>agrege un h4 con javascript</h4>" // se agrego un h4 con la etiqueta h4 dentro de un innerhtml en la variable k que guarda la clase cuarto del documento html
+
+m.className = m.className.replace("parrafos","tag-p"); // gracias al classname y la funcion replace cambiamos el nombre de la clase parrafos guardado en la variable m por una nueva clase tag-p
+
+ñ.style.fontSize = "30px"; // cambiamos el tamaño de la fuente del parrafo en el indice 2 guardada en la variable ñ , en css los metodos se escriben con - por ejemplo font-size pero en js se usa el camel case por lo que van asi fontSize
+
+// esto es lo que usaras la mayoria del tiempo para llamar elementos del documento y modificarlo a tu gusto veamos un ejemplo cambiando el fondo del documento con los 3 botones que se encuentran al principio del documento dentro del div con la clase todo
+
+function fondo(color){ // aqui seleccionamos la funcion fondo creada con el onclik en los button htmnl con el parametro color
+    if(color == "rojo"){ // aqui decimos que si el parametro color es igual al parametro rojo de html
+        z.style.backgroundColor = "red"; // aqui lo cambiamos el fondo a rojo
+        z.style.color = "black"; // aqui cambiamos el color de texto a negro
+    }
+    else if(color == "negro"){
+        z.style.backgroundColor = "black"; // aqui a negro
+        z.style.color = "white";
+    }
+    else{
+        z.style.backgroundColor = "yellow"; // aqui a amarillo
+        z.style.color = "black";
+    }
+}
